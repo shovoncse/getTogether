b26035394daf048be1018fb382f475b425aed6f8
Shovon Das <shovondas26@gmail.com>
2020-03-24 23:46:12 +0600

    database connected

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+// Database Connection
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server Started on port ${POIRT}`));
+app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
