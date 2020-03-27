78cc795a76eabf14b60b9bd96a880826be47cb4c
Shovon Das <Shovondas26@gmail.com>
2020-03-27 09:45:32 +0600

    ceate data with model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
+// const {check, validationResult} = require('express-validator/check')
+const User = require('../../models/User');
 // @route   GET api/users
 // @desc    Test Route
 // @access  Public
@@ -10,6 +11,12 @@ router.get('/', (req, res) => res.send("User Route"));
 // @desc    Test Route
 // @access  Public
 router.post('/', (req, res) => {
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        date: req.body.date+
+    });
     console.log(req.body);
     res.send("User Route");
 });
