2fef517b06cbcc1029570a10a2a9a93594bd1051
Shovon Das <Shovondas26@gmail.com>
2020-03-27 10:05:19 +0600

    Request Validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// const {check, validationResult} = require('express-validator/check')
+const {check, validationResult} = require('express-validator/check')
 const User = require('../../models/User');
 // @route   GET api/users
 // @desc    Test Route
@@ -10,13 +10,24 @@ router.get('/', (req, res) => res.send("User Route"));
 // @route   POST api/users
 // @desc    Test Route
 // @access  Public
-router.post('/', (req, res) => {
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        date: req.body.date+
-    });
+router.post('/', [
+    // name should not be empty
+    check('name', 'Name is Required').notEmpty(),
+    check('email', 'Include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({min:6})
+], (req, res) => {
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    // User.create({
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: req.body.password,
+    //     date: req.body.date+
+    // });
     console.log(req.body);
     res.send("User Route");
 });
