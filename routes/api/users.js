b715dfef83e1969239c96fc8690d01272e8a8849
Shovon Das <Shovondas26@gmail.com>
2020-03-28 09:30:49 +0600

    get avatar

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {check, validationResult} = require('express-validator/check')
 const User = require('../../models/User');
+const gravatar = require('gravatar');
 
 // @route   GET api/users
 // @desc    Test Route
@@ -26,19 +27,26 @@ router.post('/', [
 
     try {
         
+        // see if user Exists
         let user = await User.findOne({email});
         if(user){
             res.status(400).json({errors: [{msg: 'User already exists'}]});
         }
+
+        // Get users Gravatar
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        })
+        
         console.log(req.body);
         res.send("User Route");
     }catch(err){
         console.log(err.message);
         res.status(500).send('Server Error');
     }
-    // see if user Exists
 
-    // Get users Gravatar
 
     // Encrypt Password
 
