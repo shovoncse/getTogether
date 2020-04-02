973577054d74267d4d76eced5f54c4ee927a010e
Shovon Das <Shovondas26@gmail.com>
2020-04-03 00:38:30 +0600

    profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
 
 
 // @route   GET api/profile/me
 // @desc    Test Route
 // @access  Private
-router.get('/', auth, (req, res) => {
-    res.send("Profile Route")
+router.get('/me', auth, async (req, res) => {
+    try{
+        const profile = await Profile.findOne({user: req.user.id}).populate('user', ['name', 'avatar'])
+
+        if(!profile){
+            return res.status(400).json({msg: 'There is no profile'})
+        }
+
+        res.json(profile);
+
+    }catch(err){
+        console.log(err.message);
+        res.status(500).send('Server Error')
+    } 
 });
 
 
