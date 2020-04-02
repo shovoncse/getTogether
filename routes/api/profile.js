a30da3437117defb4aa1c1174cf05b14c6bfb46b
Shovon Das <Shovondas26@gmail.com>
2020-04-02 23:49:31 +0600

    my profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route   GET api/profile
+
+// @route   GET api/profile/me
 // @desc    Test Route
-// @access  Public
-router.get('/', (req, res) => res.send("Profile Route"));
+// @access  Private
+router.get('/', auth, (req, res) => {
+    res.send("Profile Route")
+});
 
 
-module.exports = router;
+module.exports = router; 
