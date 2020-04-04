a9dc4e5b57220ca4c8a7443a7dd123d517a1c511
Shovon Das <Shovondas26@gmail.com>
2020-04-04 23:46:17 +0600

    Create Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -25,5 +25,25 @@ router.get('/me', auth, async (req, res) => {
     } 
 });
 
+// @route   GET api/profile
+// @desc    Test Route
+// @access  Public
+router.post('/', auth, async (req, res) => {
+    try{
+        const profile = await Profile.findOne({user: req.user.id}).populate('user', ['name', 'avatar'])
+
+        if(!profile){
+            return res.status(400).json({msg: 'There is no profile'})
+        }
+
+        res.json(profile);
+
+    }catch(err){
+        console.log(err.message);
+        res.status(500).send('Server Error')
+    } 
+});
+
+
 
 module.exports = router; 
